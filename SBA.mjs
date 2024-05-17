f3ff8c5d5803b49497c71ccd38e1bb1068c01bc1
ebelbell <ebella2056@gmail.com>
2024-05-17 05:09:09 -0700

    Update SBA.mjs
    attempted a try/catch method with error handling.

--- SBA.mjs
+++ SBA.mjs
@@ -12,20 +12,6 @@ Utilize loops and iteration to navigate through data collections.
 Implement error handling to manage potential code failures gracefully.
 */
 
-//You will create a script that gathers data, processes it, and then outputs 
-//a consistent result as described by a specification. 
-//This is a very typical situation in industry, and this particular scenario 
-//has been modified from a real application.
-
-/*Criteria:
-Employ basic JavaScript syntax accurately.
-Implement control flow structures such as conditionals and loops effectively.
-Use arrays and objects to organize and manage data.
-Develop functions to create reusable code.
-Utilize loops and iteration to navigate through data collections.
-Implement error handling to manage potential code failures gracefully.
-*/
-
 
 // course info data object
 const courseInfo = {
@@ -86,4 +72,30 @@ function getLearnerData(courseInfo, assignmentGroup, learnerSubmission){
         }
     } 
     return result;
-}
+
+    //Check that the assignment group belongs to the course id
+    if (assignmentGroup.course_id !== courseInfo.id) {
+        throw new Error(`Assignment group ${assignmentGroup.id} belongs to the ${assignmentGroup.course_id}`);
+    }
+
+    //this try/catch method will check the error handling
+    try {
+        const courseInfo = createCourseInfo(308, "JavaScript");
+
+        const assignment = [
+            new assignmentInfo(1, "homework 1", 100), // scored out of 100
+            new assignmentInfo(2, "homework 2", 100),
+            new assignmentInfo(3, "homework 3", 100),
+        ];
+
+        const assignmentGroup = newAssignmentGroup(1, "homework", 306, 30, assignment);
+        
+        const result = getLearnerData(courseInfo, assignmentGroup, learnerSubmission);
+        console.log(result);
+    }   catch (error) {
+        console.error(error.message);
+    }
+} 
+
+
+//try/catch to check if assignmentGroup doesn't belong to its course
